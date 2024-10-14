7fcd38bce168685cc6944ade89b5038effb3c367
Pascal <pm@software-design.de>
2024-10-14 10:48:18 +0200

    js and csv

--- DevOpsCustomerView/userinterface/static/js/base.js
+++ DevOpsCustomerView/userinterface/static/js/base.js
@@ -1,4 +1,20 @@
-// Search projects
+document.addEventListener('DOMContentLoaded', function () {
+    initializeTooltips();
+
+    document.getElementById("searchInput")?.addEventListener('input', searchProjects)
+
+    document.querySelectorAll('[data-dismiss="modal"]').forEach(button => {
+        button.addEventListener('click', closeModal);
+    });
+
+    document.getElementById('loginDropdown')?.addEventListener('click', function (event) {
+        event.stopPropagation();
+        focusFirstInputField('loginDropdown');
+    });
+});
+
+
+
 function searchProjects() {
     const input = document.getElementById("searchInput").value.toUpperCase();
     const projectCards = document.querySelectorAll("#projectsHolder .card");
@@ -13,6 +29,15 @@ function searchProjects() {
     });
 }
 
+function closeModal(event) {
+    event.stopPropagation();
+    const modalElement = event.target.closest('.modal');
+    if (modalElement) {
+        const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+        modalInstance.hide();
+    }
+}
+
 function initializeTooltips() {
     const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
@@ -21,7 +46,18 @@ function initializeTooltips() {
 }
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    initializeTooltips();
-    document.getElementById("searchInput")?.addEventListener('input', searchProjects);
-});
+/**
+ * Focuses the first input field within the specified element.
+ * @param {string} elementId - The ID of the element containing the input field.
+ */
+function focusFirstInputField(elementId) {
+    const dropdown = document.getElementById(elementId);
+    if (dropdown) {
+        const emailInput = document.querySelector('input[name="email"]');
+        if (emailInput) {
+            emailInput.focus();
+        }
+    }
+}
+
+
